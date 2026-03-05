4eca970337e5fd95a958f9c990b13d97a5d32fb8
Raymundosalas <128091595+Raymundosalas@users.noreply.github.com>
2026-03-05 14:32:23 -0600

    Implement GameManager to manage game score

---
+++ GameManager.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance;
+    public int score = 0;
+    public Text scoreText;
+
+    void Awake()
+    {
+        instance = this;
+    }
+
+    public void AddScore(int points)
+    {
+        score += points;
+        scoreText.text = "Score: " + score;
+    }
+}
